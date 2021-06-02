af21ff903bcd16476bc620ac614eff94fccaa708
treeform <starplant@gmail.com>
2021-06-02 11:00:12 -0700

    Add errors test.

--- tests/test_errors.nim
+++ tests/test_errors.nim
@@ -1,1 +1,10 @@
 import jsony
+
+doAssertRaises(JsonError):
+  discard "{invalid".fromJson()
+
+doAssertRaises(JsonError):
+  discard "{a:}".fromJson()
+
+doAssertRaises(JsonError):
+  discard "1.23.23".fromJson()
