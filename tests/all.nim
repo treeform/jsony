008d45f0058a59f8431d97cd0cecfa232585c1cb
treeform <starplant@gmail.com>
2021-06-02 07:31:31 -0700

    Make JS work again.

---
+++ tests/all.nim
@@ -0,0 +1,21 @@
+# This is mainly used to test js
+# nim js -r .\tests\all.nim
+# nim cpp -r .\tests\all.nim
+
+import test_arrays
+import test_char
+import test_enums
+import test_errors
+import test_fast_numbers
+import test_json_in_json
+import test_numbers
+import test_objects
+import test_options
+import test_parseHook
+import test_sets
+import test_strings
+import test_tables
+import test_tojson
+import test_tuples
+
+echo "all tests pass"
