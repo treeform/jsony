c84565741af926ea12f41aba455c4f3b0a43d755
Jake Leahy <jake@leahy.dev>
2022-01-03 17:25:13 +1100

    Fix seq[tuple] with named tuples

--- tests/test_tuples.nim
+++ tests/test_tuples.nim
@@ -46,4 +46,27 @@ block:
   doAssert v[2] == 13.5
   doAssert v.id == 134
   doAssert v.name == "red"
+
+block:
+  type Entry = tuple[id: int, name: string, dist: float32]
+  var s = """[{"id": 134, "name": "red", "dist": 13.5}]""" 
+  var entries = s.fromJson(seq[Entry])
+  doAssert entries.len == 1
+  var v = entries[0]
+  doAssert v.dist == 13.5 
+  doAssert v[0] == 134
+  doAssert v[1] == "red"
+  doAssert v[2] == 13.5
+  doAssert v.id == 134
+  doAssert v.name == "red"
   doAssert v.dist == 13.5
+  
+type EntryForHook = tuple[id: int, name: string]
+proc postHook(entry: var EntryForHook) =
+  entry.id = 42
+  
+block:
+  var s = """{"id": 6, "name": "red"}"""
+  var v = s.fromJson(EntryForHook)
+  doAssert v.id == 42
+  doAssert v.name == "red"
