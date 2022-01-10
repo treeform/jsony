41a9fe026d04bcab0ddceb8d7a4d20f9540afe32
Andre von Houck <starplant@gmail.com>
2022-01-10 22:01:45 +0000

    Add a parse hook example for Non unique / double keys in json.

--- tests/test_parseHook.nim
+++ tests/test_parseHook.nim
@@ -1,4 +1,4 @@
-import jsony, strutils, tables, times
+import jsony, strutils, tables, times, json
 
 type Fraction = object
   numerator: int
@@ -46,3 +46,67 @@ proc parseHook(s: string, i: var int, v: var seq[Entry]) =
 let s = data.fromJson(seq[Entry])
 doAssert type(s) is seq[Entry]
 doAssert $s == """@[(id: "1", count: 12, filled: 11), (id: "2", count: 66, filled: 0), (id: "3", count: 99, filled: 99)]"""
+
+type Entry2 = object
+  id: int
+  pre: int
+  post: int
+  kind: string
+
+let data2 = """{
+  "id": 3444,
+  "changes": [1, 2, "hi"]
+}"""
+
+proc parseHook(s: string, i: var int, v: var Entry2) =
+  var entry: JsonNode
+  parseHook(s, i, entry)
+  v = Entry2()
+  v.id = entry["id"].getInt()
+  v.pre = entry["changes"][0].getInt()
+  v.post = entry["changes"][1].getInt()
+  v.kind = entry["changes"][2].getStr()
+
+let s2 = data2.fromJson(Entry2)
+doAssert type(s2) is Entry2
+doAssert $s2 == """(id: 3444, pre: 1, post: 2, kind: "hi")"""
+
+# Non unique / double keys in json
+# https://forum.nim-lang.org/t/8787
+type Header = object
+  key: string
+  value: string
+proc parseHook(s: string, i: var int, v: var seq[Header]) =
+  eatChar(s, i, '{')
+  while i < s.len:
+    eatSpace(s, i)
+    if i < s.len and s[i] == '}':
+      break
+    var key, value: string
+    parseHook(s, i, key)
+    eatChar(s, i, ':')
+    parseHook(s, i, value)
+    v.add(Header(key: key, value: value))
+    eatSpace(s, i)
+    if i < s.len and s[i] == ',':
+      inc i
+    else:
+      break
+  eatChar(s, i, '}')
+
+let data3 = """{
+  "Cache-Control": "private, max-age=0d",
+  "Content-Encoding": "brd",
+  "Set-Cookie": "name=valued",
+  "Set-Cookie": "name=value; name2=value2; name3=value3d"
+}"""
+
+let headers = data3.fromJson(seq[Header])
+doAssert headers[0].key == "Cache-Control"
+doAssert headers[0].value == "private, max-age=0d"
+doAssert headers[1].key == "Content-Encoding"
+doAssert headers[1].value == "brd"
+doAssert headers[2].key == "Set-Cookie"
+doAssert headers[2].value == "name=valued"
+doAssert headers[3].key == "Set-Cookie"
+doAssert headers[3].value == "name=value; name2=value2; name3=value3d"
