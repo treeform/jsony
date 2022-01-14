08f719d48de79f5ad27e7cc101f11fc3e9eed3eb
treeform <starplant@gmail.com>
2022-01-14 10:14:59 -0800

    Fix for utf16 surrogates.

--- tests/test_strings.nim
+++ tests/test_strings.nim
@@ -1,4 +1,4 @@
-import jsony, json
+import json, jsony
 
 block:
   var s = """ "hello" """
@@ -12,7 +12,7 @@ block:
   var v = s.fromJson(string)
   doAssert v == "new\nline"
   doAssert v.toJson().fromJson(string) == v
-  echo v.toJson().fromJson().toJson().fromJson()
+  doAssert v.toJson().fromJson().toJson().fromJson() == newJString("new\nline")
 
 block:
   var s = """ "quote\"inside" """
@@ -31,3 +31,25 @@ block:
   var v = s.fromJson(string)
   doAssert v == "unicode: \u0020 \u0F88 \u1F21"
   doAssert v.toJson().fromJson(string) == v
+
+block:
+  # https://github.com/treeform/jsony/issues/45
+  # A string with 🔒 emoji encoded both as normal UTF-8 and as a surrogate pair
+  type
+    TestObj = object
+      content: string
+  let
+    raw = """{"content":"\uD83D\uDD12🔒"}"""
+    parsed = raw.fromJson(TestObj)
+    parsedStd = parseJson(raw).to(TestObj)
+  echo "jsony - ", parsed.content
+  echo "std/json - ", parsedStd.content
+  doAssert parsed.content == parsedStd.content
+
+  let
+    raw2 = """{"content":"\u00A1\uD835\uDC7D\uD835\uDC96\uD835\uDC86\uD835\uDC8D\uD835\uDC97\uD835\uDC86\uD835\uDC8F \uD835\uDC8F\uD835\uDC96\uD835\uDC86\uD835\uDC94\uD835\uDC95\uD835\uDC93\uD835\uDC90\uD835\uDC94 \uD835\uDC89\uD835\uDC8A\uD835\uDC8F\uD835\uDC84\uD835\uDC89\uD835\uDC82\uD835\uDC94!"}"""
+    parsed2 = raw2.fromJson(TestObj)
+    parsedStd2 = parseJson(raw2).to(TestObj)
+  echo "jsony - ", parsed2.content
+  echo "std/json - ", parsedStd2.content
+  doAssert parsed2.content == parsedStd2.content
