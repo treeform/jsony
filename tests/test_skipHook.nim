c846af20d16a5d49a17ee6481bb4f9692b86b431
Pietro Peterlongo <pietro.peterlongo@gmail.com>
2023-03-14 04:46:26 +0100

    Inessential test change to test CI

--- tests/test_skipHook.nim
+++ tests/test_skipHook.nim
@@ -12,6 +12,6 @@ type
 proc skipHook(T: typedesc[Foo], key: static string): bool =
   key in ["password", "conn"]
 
-let v = Foo(a:1, password: "12345", b:0.5, conn: Conn(id: 1))
+let v = Foo(a:1, password: "12345", b:0.6, conn: Conn(id: 1))
 doAssert v.toJson() ==
-  """{"a":1,"b":0.5}"""
+  """{"a":1,"b":0.6}"""
