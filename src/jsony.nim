371c401b52978ebac10fa3da895f48c0a9b7d28e
Pietro Peterlongo <pietro.peterlongo@gmail.com>
2023-06-12 21:08:09 +0200

    Revert "this fails with Error: cannot evaluate at compile time: k"
    This reverts commit f51a5f04ab86826adc0131bfa317761c5b36cd22.

--- src/jsony.nim
+++ src/jsony.nim
@@ -712,10 +712,6 @@ template dumpKey(s: var string, v: string) =
   const v2 = v.toJson() & ":"
   s.add v2
 
-template dumpKey(s: var string, v: enum) =
-  const v2 = '"' & $v & '"' & ":"
-  s.add v2
-
 proc dumpHook*(s: var string, v: char) =
   s.add '"'
   s.add v
@@ -766,7 +762,7 @@ proc dumpHook*(s: var string, v: object) =
     for k, e in v.pairs:
       if i > 0:
         s.add ','
-      s.dumpKey(k)
+      s.dumpHook(k)
       s.add ':'
       s.dumpHook(e)
       inc i
